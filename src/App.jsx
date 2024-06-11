ea7cd0ec6f6aaca446a63d56f11b26bce600cbb9
Rossignol Alex <alexnw33910@gmail.com>
2024-06-11 15:43:12 +0200

    Check props

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,20 @@ import './App.css'
 
 function App() {
 
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
   return (
     <>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]} />
     </>
   )
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,16 +1,5 @@
-function PokemonCard () {
-    const pokemonList = [
-        {
-          name: "bulbasaur",
-          imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-        },
-        {
-          name: "mew",
-        },
-      ];
+function PokemonCard ({pokemon}) {
       
-    const pokemon = pokemonList[0];
     return <figure>
         {pokemon.imgSrc 
         ? (<img src={pokemon.imgSrc} alt="bulbasaur"/>)
