3dc706102e700ccff0ce1ae49da7093d5870785a
Rossignol Alex <alexnw33910@gmail.com>
2024-06-11 23:33:35 +0200

    Added button previous and next

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,47 @@ import './App.css'
 
 function App() {
 
+  const [pokemonIndex, setpokemonIndex] = useState(0);
+  
+  const clickMoins = () =>{
+    setpokemonIndex((index) => index - 1);
+  };
+  const clickPlus = () =>{
+    setpokemonIndex((index) => index + 1);
+  };
   const pokemonList = [
     {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
 
   return (
     <>
-      <PokemonCard pokemon={pokemonList[0]} />
+      { pokemonIndex > 0 &&
+      (<button type='button' onClick={clickMoins}>Précédent</button>  )}
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      { pokemonIndex < pokemonList.length - 1 &&
+      (<button type='button' onClick={clickPlus}>Suivant</button> )}
     </>
   )
 }
