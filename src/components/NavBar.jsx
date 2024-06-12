2bf3354b6923c986d6e21d7030d7511d341b5999
Rossignol Alex <alexnw33910@gmail.com>
2024-06-12 15:41:00 +0200

    Used map()

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,10 +7,13 @@ export default function NavBar({pokemonIndex, setpokemonIndex, pokemonList}) {
       };
     return (
     <>
-        { pokemonIndex > 0 &&
+      {pokemonList.map((pokemon, index) => {
+        return <button key={pokemon.name} onClick={() => setpokemonIndex(index)}>{pokemon.name}</button>
+      })}
+        {/* { pokemonIndex > 0 &&
             (<button type='button' onClick={clickMoins}>Précédent</button>  )}
         { pokemonIndex < pokemonList.length - 1 &&
-            (<button type='button' onClick={clickPlus}>Suivant</button> )}
+            (<button type='button' onClick={clickPlus}>Suivant</button> )} */}
     </>
     );
 };
